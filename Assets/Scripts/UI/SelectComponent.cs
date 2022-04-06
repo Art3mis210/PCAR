5bb31ec4caf4326d3557c687c7d4239b426a6807
Art3mis210 <77666553+Art3mis210@users.noreply.github.com>
2022-04-06 23:45:41 +0530

    CHanged UI

--- Assets/Scripts/UI/SelectComponent.cs
+++ Assets/Scripts/UI/SelectComponent.cs
@@ -1,17 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class SelectComponent : MonoBehaviour
 {
-    public GameObject MotherboardList;
-    public GameObject CPUList;
-    public GameObject GPUList;
-    public GameObject RAMList;
-    public GameObject HDDSSDList;
-    public GameObject PowerSupplyList;
-    public GameObject CabinetList;
+    public GameObject[] PCComponentList;
+    public int CurrentSection;
+    public Button BuildButton;
+    public Button NextButton;
     public GameObject Build;
+    public GameObject BuildList;
 
     private PCComponent Motherboard;
     private PCComponent CPU;
@@ -28,70 +27,229 @@ public class SelectComponent : MonoBehaviour
     public BuildSpawn HDDSSDSpawn;
     public BuildSpawn PowerSupplySpawn;
     public BuildSpawn CabinetSpawn;
-
+    private void OnEnable()
+    {
+        CurrentSection = 0;
+        PCComponentList[0].SetActive(true);
+        if (Motherboard != null)
+        {
+            Motherboard.GetComponentInChildren<Button>(true).transform.gameObject.SetActive(true);
+            Motherboard = null;
+        }
+        if(CPU!=null)
+        {
+            CPU.GetComponentInChildren<Button>(true).transform.gameObject.SetActive(true);
+            CPU = null;
+        }
+        if (GPU != null)
+        {
+            GPU.GetComponentInChildren<Button>(true).transform.gameObject.SetActive(true);
+            GPU = null;
+        }
+        if (RAM != null)
+        {
+            RAM.GetComponentInChildren<Button>(true).transform.gameObject.SetActive(true);
+            RAM = null;
+        }
+        if (PowerSupply != null)
+        {
+            PowerSupply.GetComponentInChildren<Button>(true).transform.gameObject.SetActive(true);
+            PowerSupply = null;
+        }
+        if (Cabinet != null)
+        {
+            Cabinet.GetComponentInChildren<Button>(true).transform.gameObject.SetActive(true);
+            Cabinet = null;
+        }
+        for(int i=BuildList.transform.childCount-1 ; i>=0;i--)
+        {
+            Destroy(BuildList.transform.GetChild(i).gameObject);
+        }
+        BuildButton.interactable = false;
+    }
     public void SelectMotherboard(PCComponent Motherboard)
     {
-        this.Motherboard = Motherboard;
-        foreach(CPU cpu in CPUList.GetComponentsInChildren<CPU>())
+        if(this.Motherboard!=null)
         {
-            if (cpu.CPUSlot != Motherboard.GetComponent<Motherboard>().CPUSlot)
+            this.Motherboard.GetComponentInChildren<Button>(true).transform.gameObject.SetActive(true);
+        }
+        this.Motherboard = Motherboard;
+        this.Motherboard.GetComponentInChildren<Button>().transform.gameObject.SetActive(false);
+        foreach (CPU cpu in PCComponentList[1].GetComponentsInChildren<CPU>(true))
+        {
+            if (cpu.CPUSlot == Motherboard.GetComponent<Motherboard>().CPUSlot)
+                cpu.transform.gameObject.SetActive(true);
+            else
                 cpu.transform.gameObject.SetActive(false);
         }
-        foreach (RAM ram in RAMList.GetComponentsInChildren<RAM>())
+        foreach (RAM ram in PCComponentList[2].GetComponentsInChildren<RAM>(true))
         {
-            if (ram.RamType!= Motherboard.GetComponent<Motherboard>().RamType)
+            if (ram.RamType== Motherboard.GetComponent<Motherboard>().RamType)
+                ram.transform.gameObject.SetActive(true);
+            else
                 ram.transform.gameObject.SetActive(false);
         }
-        MotherboardList.SetActive(false);
-        CPUList.SetActive(true);
+        if(CPU!=null)
+        {
+            CPU.GetComponentInChildren<Button>(true).transform.gameObject.SetActive(true);
+            CPU = null;
+        }
+        if (GPU != null)
+        {
+            GPU.GetComponentInChildren<Button>(true).transform.gameObject.SetActive(true);
+            GPU = null;
+        }
+        if (RAM != null)
+        {
+            RAM.GetComponentInChildren<Button>(true).transform.gameObject.SetActive(true);
+            RAM = null;
+        }
+        NextButton.interactable = true;
+        //MotherboardList.SetActive(false);
+        //CPUList.SetActive(true);
     }
     public void SelectCPU(PCComponent CPU)
     {
+        if (this.CPU != null)
+        {
+            this.CPU.GetComponentInChildren<Button>(true).transform.gameObject.SetActive(true);
+        }
         this.CPU = CPU;
-        CPUList.SetActive(false);
-        GPUList.SetActive(true);
+        this.CPU.GetComponentInChildren<Button>().transform.gameObject.SetActive(false);
+        // CPUList.SetActive(false);
+        //GPUList.SetActive(true);
+        NextButton.interactable = true;
     }
     public void SelectGPU(PCComponent GPU)
     {
+        if (this.GPU != null)
+        {
+            this.GPU.GetComponentInChildren<Button>(true).transform.gameObject.SetActive(true);
+        }
         this.GPU = GPU;
-        GPUList.SetActive(false);
-        RAMList.SetActive(true);
+        this.GPU.GetComponentInChildren<Button>().transform.gameObject.SetActive(false);
+        //GPUList.SetActive(false);
+        //RAMList.SetActive(true);
+        NextButton.interactable = true;
     }
     public void SelectRAM(PCComponent RAM)
     {
+        if (this.RAM != null)
+        {
+            this.RAM.GetComponentInChildren<Button>(true).transform.gameObject.SetActive(true);
+        }
         this.RAM = RAM;
-        RAMList.SetActive(false);
-        HDDSSDList.SetActive(true);
+        this.RAM.GetComponentInChildren<Button>().transform.gameObject.SetActive(false);
+        //RAMList.SetActive(false);
+        //HDDSSDList.SetActive(true);
+        NextButton.interactable = true;
     }
     public void SelectHDDSSD(PCComponent HDDSSD)
     {
+        if (this.HDDSSD != null)
+        {
+            this.HDDSSD.GetComponentInChildren<Button>(true).transform.gameObject.SetActive(true);
+        }
         this.HDDSSD = HDDSSD;
-        HDDSSDList.SetActive(false);
-        PowerSupplyList.SetActive(true);
+        this.HDDSSD.GetComponentInChildren<Button>().transform.gameObject.SetActive(false);
+        //HDDSSDList.SetActive(false);
+        //PowerSupplyList.SetActive(true);
+        NextButton.interactable = true;
     }
     public void SelectPowerSupply(PCComponent PowerSupply)
     {
+        if (this.PowerSupply != null)
+        {
+            this.PowerSupply.GetComponentInChildren<Button>(true).transform.gameObject.SetActive(true);
+        }
         this.PowerSupply = PowerSupply;
-        PowerSupplyList.SetActive(false);
-        CabinetList.SetActive(true);
+        this.PowerSupply.GetComponentInChildren<Button>().transform.gameObject.SetActive(false);
+        //PowerSupplyList.SetActive(false);
+        //CabinetList.SetActive(true);
+        NextButton.interactable = true;
     }
     public void SelectCabinet(PCComponent Cabinet)
     {
+        if (this.Cabinet != null)
+        {
+            this.Cabinet.GetComponentInChildren<Button>(true).transform.gameObject.SetActive(true);
+        }
         this.Cabinet = Cabinet;
-        CabinetList.SetActive(true);
-        AssignBuildPrefabs();
-        Build.SetActive(true);
-        transform.parent.gameObject.SetActive(false);
+        this.Cabinet.GetComponentInChildren<Button>().transform.gameObject.SetActive(false);
+
+        //Build.SetActive(true);
+        //transform.parent.gameObject.SetActive(false);
     }
-    void AssignBuildPrefabs()
+    public void AssignBuildPrefabs()
     {
         MotherboardSpawn.SpawnPrefab = Motherboard.ComponentPrefab;
+        Instantiate(Motherboard.gameObject, BuildList.transform);
         CPUSpawn.SpawnPrefab = CPU.ComponentPrefab;
+        Instantiate(CPU.gameObject, BuildList.transform);
         GPUSpawn.SpawnPrefab = GPU.ComponentPrefab;
+        Instantiate(GPU.gameObject, BuildList.transform);
         RAMSpawn.SpawnPrefab = RAM.ComponentPrefab;
+        Instantiate(RAM.gameObject, BuildList.transform);
         HDDSSDSpawn.SpawnPrefab = HDDSSD.ComponentPrefab;
+        Instantiate(HDDSSD.gameObject, BuildList.transform);
         PowerSupplySpawn.SpawnPrefab = PowerSupply.ComponentPrefab;
+        Instantiate(PowerSupply.gameObject, BuildList.transform);
         CabinetSpawn.SpawnPrefab = Cabinet.ComponentPrefab;
+        Instantiate(Cabinet.gameObject, BuildList.transform);
+
+    }
+    public void NextSection()
+    {
+        if(CurrentSection<=PCComponentList.Length-1)
+        {
+            if (CurrentSection == 0 && Motherboard == null)
+            {
+                NextButton.interactable = false;
+            }
+            else if (CurrentSection == 1 && CPU == null)
+            {
+                NextButton.interactable = false;
+            }
+            else if (CurrentSection == 2 && GPU == null)
+            {
+                NextButton.interactable = false;
+            }
+            else if (CurrentSection == 3 && RAM == null)
+            {
+                NextButton.interactable = false;
+            }
+            else if (CurrentSection == 4 && HDDSSD == null)
+            {
+                NextButton.interactable = false;
+            }
+            else if (CurrentSection == 5 && PowerSupply == null)
+            {;
+                NextButton.interactable = false;
+            }
+            else if (CurrentSection == 6 && Cabinet == null)
+            {
+                NextButton.interactable = false;
+            }
+            else if(CurrentSection<6)
+            {
+                PCComponentList[CurrentSection].SetActive(false);
+                CurrentSection += 1;
+                PCComponentList[CurrentSection].SetActive(true);
+            }
+            else
+            {
+                BuildButton.interactable = true;
+            }
+        }
+    }
+    public void PreviousSection()
+    {
+        if (CurrentSection > 0)
+        {
+            PCComponentList[CurrentSection].SetActive(false);
+            CurrentSection -= 1;
+            PCComponentList[CurrentSection].SetActive(true);
+        }
     }
 
 }
